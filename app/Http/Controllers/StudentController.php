2c633407039d571a121f39a0a403745719a4e7e5
juan rengifo <juancpardo1212@gmail.com>
2021-02-03 11:38:08 -0500

    create controller student and add function edit, update

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+class StudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit()
+    {
+        $data_student = Auth::user();
+
+        if(Auth::user()->typeUser=='Estudiante'){
+        return view('students.edit', compact('data_student'));
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, User $data_student)
+    {
+        $data_student->name = $request->name;
+        $data_student->last_name = $request->last_name;
+        $data_student->email = $request->email;
+        $data_student->phoneNumber = $request->phoneNumber;
+        $data_student->address = $request->address;
+        $data_student->addcity_residenceress = $request->city_residence;
+        $data_student->hometown = $request->hometown;
+        $data_student->nationality = $request->nationality;
+
+        $data_student->save();
+
+        return view('students.edit', compact('data_student'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
