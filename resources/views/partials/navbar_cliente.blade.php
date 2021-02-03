e9b696e027794954e2a2469b665a4de37046f3f1
juan rengifo <juancpardo1212@gmail.com>
2021-02-03 16:37:15 -0500

    [FIX] variable city_residence

--- resources/views/partials/navbar_cliente.blade.php
+++ resources/views/partials/navbar_cliente.blade.php
@@ -1,9 +1,4 @@
 <header>
-    <div class="header-container">
-        <a class="logo" href="/">
-            <img src="/img/new_logo.png" alt="">
-        </a>
-    </div>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand" href="#"></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -12,14 +7,8 @@
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Cómo funciona<span class="sr-only">(current)</span></a>
-                </li>
                 <li class="nav-item">
                     <a class="nav-link" href="{{ route('student.edit') }}">Mi perfil</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Servicios de reparación</a>
                 </li>
             </ul>
         </div>
