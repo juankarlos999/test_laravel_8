9b1f6ba00d8d56c8ec05b41efedbd8f7f4f5e25e
juan rengifo <juancpardo1212@gmail.com>
2021-02-02 20:30:39 -0500

    form register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -14,23 +14,33 @@
                             <label for="typeUser" class="col-md-4 col-form-label text-md-right">{{ __('Rol') }}</label>
                             <div class="col-md-6">
                                 <select id="typeUser" class="form-control" name="typeUser" autofocus required>
-                                    <option value="Usuario" selected>Usuario</option>
+                                    <option value="Usuario" name="Rol" >Estudiante</option>
+                                    <option value="Administrador" name="Rol" >Administrador</option>
                                 </select>
                             </div>
                         </div>
-                                <div class="form-group row">
-                                    <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Nombre completo') }}</label> 
-
-                                    <div class="col-md-6">
-                                        <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                        @error('name')
-                                            <span class="invalid-feedback" role="alert">
-                                                <strong>{{ $message }}</strong>
-                                            </span>
-                                        @enderror
-                                    </div>
-                                </div>
+                        <div class="form-group row">
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Nombre') }}</label> 
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="last_name" class="col-md-4 col-form-label text-md-right">{{ __('Apellidos') }}</label> 
+                            <div class="col-md-6">
+                                <input id="last_name" type="text" class="form-control @error('last_name') is-invalid @enderror" name="last_name" value="{{ old('last_name') }}" required autocomplete="last_name" autofocus>
+                                @error('last_name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
 
                                 <div class="form-group row">
                                     <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Correo Electronico') }}</label>
@@ -46,38 +56,56 @@
                                     </div>
                                 </div>
                                 <div class="form-group row">
-                                <label for="telefono1"  class="col-md-4 col-form-label text-md-right">Telefono principal</label>
+                                <label for="telefono1"  class="col-md-4 col-form-label text-md-right">Telefono</label>
                                 <div class="col-md-6">
-                                    <input type="tel" name="phoneNumber1" id="telefono1" class="form-control" pattern="[0-9]{10}" required>
+                                    <input type="tel" name="phoneNumber" id="telefono1" class="form-control" pattern="[0-9]{10}" required>
                                 </div>
                             </div>
-            
-                            <div class="form-group row">
-                                <label for="telefono2" class="col-md-4 col-form-label text-md-right">Telefono secundario</label>
-                                <div class="col-md-6">
-                                    <input type="tel" name="phoneNumber2" id="telefono2" class="form-control" pattern="[0-9]{10}" required>
-                                </div>       
-                            </div>
-
                             <div class="form-group row">
                                 <label for="direccion"  class="col-md-4 col-form-label text-md-right">Direccion</label>
                                 <div class="col-md-6">
                                     <input type="text" name="address" id="direccion" class="form-control" required>     
                                 </div>   
                             </div>
-
                             <div class="form-group row">
-                                <label for="barrio" class="col-md-4 col-form-label text-md-right">Barrio</label>
+                                <label for="city_residence" class="col-md-4 col-form-label text-md-right">Ciudad de residencia</label>
                                 <div class="col-md-6">
-                                <select id="barrio" name="neighborhood" class="form-control" autofocus required>
-                                    <option selected></option>
-                                    <option value="Restrepo">Restrepo</option>
-                                    <option value="Suba Rincon">Suba Rincon</option>
-                                    <option value="Teusaquillo">Teusaquillo</option>
-                                </select>
-                                </div>
+                                    <input type="text" name="city_residence" id="city_residence" class="form-control" required>     
+                                </div> 
                             </div>
-
+                            <div class="form-group row">
+                                <label for="hometown" class="col-md-4 col-form-label text-md-right">Ciudad de Origen</label>
+                                <div class="col-md-6">
+                                    <input type="text" name="hometown" id="hometown" class="form-control" required>     
+                                </div> 
+                            </div>
+                            <div class="form-group row">
+                                <label for="nationality" class="col-md-4 col-form-label text-md-right">Nacionalidad</label>
+                                <div class="col-md-6">
+                                    <input type="text" name="nationality" id="nationality" class="form-control" required>     
+                                </div> 
+                            </div>
+                            <hr>
+                            <h3>Datos del programa</h3>
+                            <div class="form-group row">
+                                <label for="program_name" class="col-md-4 col-form-label text-md-right">Nombre del programa</label>
+                                <div class="col-md-6">
+                                    <input type="text" name="program_name" id="program_name" class="form-control" required>     
+                                </div> 
+                            </div>
+                            <div class="form-group row">
+                                <label for="school" class="col-md-4 col-form-label text-md-right">Facultad</label>
+                                <div class="col-md-6">
+                                    <input type="text" name="school" id="school" class="form-control" required>     
+                                </div> 
+                            </div>
+                            <div class="form-group row">
+                                <label for="campus" class="col-md-4 col-form-label text-md-right">Campus</label>
+                                <div class="col-md-6">
+                                    <input type="text" name="campus" id="campus" class="form-control" required>     
+                                </div> 
+                            </div>
+                            <hr>
                                 <div class="form-group row">
                                     <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
