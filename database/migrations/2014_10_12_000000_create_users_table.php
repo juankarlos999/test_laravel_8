5009928e48c0a087e0dd83a01dd7484e3941c0f4
juan rengifo <juancpardo1212@gmail.com>
2021-02-08 06:58:36 -0500

    edit function create user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->unsignedInteger('role_id')->nullable();
-            $table->foreign('role_id')->references('id')->on('roles');
+            $table->integer('role_id');
             $table->string('name');
             $table->string('last_name');
             $table->string('email')->unique();
