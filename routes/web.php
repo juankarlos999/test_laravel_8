ca946e9f7577549ed111c9454dcf16f8e5a53c4c
juan rengifo <juancpardo1212@gmail.com>
2021-02-08 19:58:37 -0500

    create route post form register user

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\StudentController;
+use App\Http\Controllers\AddressController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -20,7 +21,9 @@ Route::get('/', [PageController::class, 'home']);
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 
 /* Students*/
-
-Route::get('/estudiante', [StudentController::class, 'edit'])->middleware('auth')->name('student.edit');
+Route::get('/perfil-estudiante', [StudentController::class, 'edit'])->middleware('auth')->name('student.edit');
 
 Route::put('/actualizado-estudiante/{id}', [StudentController::class, 'update'])->middleware('auth')->name('student.update');
+
+/* Address */
+Route::post('/perfil-estudiante', [AddressController::class, 'create'])->name('address.create');
