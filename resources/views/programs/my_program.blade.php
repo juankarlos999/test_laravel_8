60c3847db0abbf023b1dd033a426aac0174894ea
juan rengifo <juancpardo1212@gmail.com>
2021-02-09 17:08:36 -0500

    edit options programs

--- resources/views/programs/my_program.blade.php
+++ resources/views/programs/my_program.blade.php
@@ -15,40 +15,38 @@
                     <form method="" action="#">
                        <!-- @csrf -->
                        <!-- Programs -->
-                        @foreach($data_program as $key =>$program)
                         <div class="form-group row">
                             <label for="name_program" class="col-md-4 col-form-label text-md-right">{{ __('Nombre del programa') }}</label>
                             <div class="col-md-6">
                                 <select id="name_program" class="form-control" name="name_program" autofocus required>
+                                @foreach($data_program as $key =>$program)
                                     <option value="{{ $program->id }}" selected>{{ $program->name_program }}</option>
+                                @endforeach
                                 </select>
                             </div>
                         </div>
-                        @endforeach
                         <!-- Faculty -->
-                        @foreach($data_program as $key =>$faculty)
                         <div class="form-group row">
                                 <label for="faculty" class="col-md-4 col-form-label text-md-right">Facultad</label>
                                 <div class="col-md-6">
                                 <select id="faculty" name="faculty" class="form-control" autofocus required>
-                                    <option selected></option>
+                                @foreach($data_program as $key =>$faculty)
                                     <option value="{{ $faculty->id }}">{{ $faculty->name_program }}</option>
+                                @endforeach
                                 </select>
                                 </div>
                             </div>
-                        @endforeach
                         <!-- Campus -->
-                        @foreach($data_program as $key =>$campus)
                             <div class="form-group row">
                                 <label for="campus" class="col-md-4 col-form-label text-md-right">Campus</label>
                                 <div class="col-md-6">
                                 <select id="campus" name="campus" class="form-control" autofocus required>
-                                    <option selected></option>
+                                @foreach($data_program as $key =>$campus)
                                     <option value="{{ $faculty->id }}">{{ $campus->campus }}</option>
+                                @endforeach
                                 </select>
                                 </div>
                             </div>
-                        @endforeach
                                 <div class="form-group row mb-0">
                                     <div class="col-md-6 offset-md-4">
                                         <button type="submit" class="btn btn-primary">
