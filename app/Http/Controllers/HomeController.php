c1069b498dce5814156bd5086932eb6c51f45cf8
juan rengifo <juancpardo1212@gmail.com>
2021-02-09 17:48:38 -0500

    validate type rol user

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -32,9 +32,9 @@ class HomeController extends Controller
 
         if(is_null($user->address)){
             return view('auth.register');
-        }elseif($rol_user->roles->role_id == 1){
+        }elseif($rol_user->role_id == 1){
             return view('students.inicio');
-        }elseif($rol_user->roles->role_id == 2){
+        }elseif($rol_user->role_id == 2){
             return view('administrator.inicio');
         }
     }
