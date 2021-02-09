21e6f8832c484cc9a66b125686d4af61d7fe5b4c
juan rengifo <juancpardo1212@gmail.com>
2021-02-09 18:47:48 -0500

    add class Auth

--- app/Http/Controllers/AdministratorController.php
+++ app/Http/Controllers/AdministratorController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class AdministratorController extends Controller
 {
